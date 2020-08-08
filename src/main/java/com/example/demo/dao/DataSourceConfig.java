2071eabbb2349cac1f7ffb10c47dbfd832a44bfc
jiangbaoming <837639305@qq.com>
2020-08-08 21:13:04 +0800

    init

---
+++ src/main/java/com/example/demo/dao/DataSourceConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.dao;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "mysqlDataSource")
+    @Qualifier("mysqlDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.mysql")
+    public DataSource mysql(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "oracleDataSource")
+    @Qualifier("oracleDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.oracle")
+    public DataSource oracle(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "mysqlJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(@Qualifier("mysqlDataSource")DataSource mysqlDataSource){
+        return new JdbcTemplate(mysqlDataSource);
+    }
+
+    @Bean(name = "oracleJdbcTemplate")
+    public JdbcTemplate secondaryJdbcTemplate(@Qualifier("oracleDataSource")DataSource oracleDataSource){
+        return new JdbcTemplate(oracleDataSource);
+    }
+}
